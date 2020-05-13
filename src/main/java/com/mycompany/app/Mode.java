059dd762574a8669d72b8202eed9af36d3073fc4
jaycrowww <jcroft1@student.unimelb.edu.au>
2020-05-13 23:10:49 +1000

    Placing enum into its own file

---
+++ src/main/java/com/mycompany/app/Mode.java
@@ -0,0 +1,7 @@
+package com.mycompany.app;
+
+/* Referencing method of travel in Google Maps */
+// Alex recommended Enums be separate
+public enum Mode {
+        WALKING, DRIVING, CYCLING, TRANSIT
+}
